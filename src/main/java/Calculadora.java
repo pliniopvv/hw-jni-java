6426106f81051c5d994256c04b4eaeb16da4d1d7
gangss <gangss2@hotmail.com>
2023-11-07 21:07:13 -0300

    bugado

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora {
+
+    static {
+        System.loadLibrary("calculadora");
+    }
+
+    public native int soma(int a, int b);
+
+    public static void main(String args[]) {
+        var calculadora = new Calculadora();
+        int resultado = calculadora.soma(1,2);
+
+        System.out.println("O resultado da soma é: " + resultado);
+    }
+
+}
